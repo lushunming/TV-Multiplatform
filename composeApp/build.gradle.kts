db9295413bf5d82eda11f1ceff55473066c697d3
lushunming <1357197829@qq.com>
2026-09-24 23:33:54 +0800

    conveyor

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -18,6 +18,10 @@ room {
 dependencies {
 //    implementation("io.ktor:ktor-server-cors:3.1.2")
     ksp(libs.roomCompiler)
+    linuxAmd64(compose.desktop.linux_x64)
+    macAmd64(compose.desktop.macos_x64)
+    macAarch64(compose.desktop.macos_arm64)
+    windowsAmd64(compose.desktop.windows_x64)
 }
 version = "1.2.0"
 
